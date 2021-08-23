134c327eecddf68d975c7ca35b2e6f3a5cf92154
Dominic Maas <dominic.maas@live.com>
2021-08-24 09:04:21 +1200

    A bit of cleaning up, start work on setting config

--- src/CameraController.cpp
+++ src/CameraController.cpp
@@ -4,7 +4,7 @@
 
 #include "CameraController.h"
 
-bool CameraController::connect() {
+ControllerResponse CameraController::connect() {
     // TODO: What happens if connect is called twice? Clean up old resources
 
     // Required context and camera init
@@ -14,22 +14,74 @@ bool CameraController::connect() {
     gp_camera_new(&this->_camera);
     int init_res = gp_camera_init(this->_camera, this->_context);
     if (init_res != 0) {
-        std::cout << "Unable to connect to camera: " << gp_result_as_string(init_res) << std::endl;
-        return false;
+        auto message = fmt::format("Unable to connect to camera. Result: {}", gp_result_as_string(init_res));
+
+        fmt::print("{}\n", message);
+        return ControllerResponse { false, message };
     }
 
-    CameraText camText;
-    gp_camera_get_about(this->_camera, &camText, this->_context);
+    CameraText cam_text;
+    gp_camera_get_about(this->_camera, &cam_text, this->_context);
 
-    std::cout << "Camera Text: " << camText.text << std::endl;
-    return true;
+    std::cout << "Camera Text: " << cam_text.text << std::endl;
+    return ControllerResponse {  };
 }
 
-bool CameraController::disconnect() {
+ControllerResponse CameraController::disconnect() {
     if (this->_camera != nullptr) {
         gp_camera_exit(this->_camera, this->_context);
     }
 
-
-    return false;
+    return ControllerResponse {  };
 }
+
+ControllerResponse CameraController::set_config_item(const char *name, const void *value) {
+    // Keep track of return codes
+    int res;
+
+    // Get the config
+    CameraWidget *config;
+    res = gp_camera_get_config(this->_camera, &config, this->_context);
+    if (res != 0) {
+        auto message = fmt::format("Unable to get camera config. Result: {}", gp_result_as_string(res));
+
+        fmt::print("{}\n", message);
+        return ControllerResponse { false, message };
+    }
+
+    // Get the widget
+    CameraWidget *widget;
+    res = gp_widget_get_child_by_name(config, name, &widget);
+    if (res != 0) {
+        auto message = fmt::format("Unable to get specified camera config ({}). Result: {}", name, gp_result_as_string(res));
+
+        fmt::print("{}\n", message);
+        return ControllerResponse { false, message };
+    }
+
+    // Attempt to set the value
+    res = gp_widget_set_value(widget, value);
+    if (res != 0) {
+        auto message = fmt::format("Unable to set camera config ({}) value. Result: {}", name, gp_result_as_string(res));
+
+        fmt::print("{}\n", message);
+        return ControllerResponse { false, message };
+    }
+
+    // Attempt to set the config
+    res = gp_camera_set_config(this->_camera, config, this->_context);
+    if (res != 0) {
+        auto message = fmt::format("Unable to set camera config. Result: {}", gp_result_as_string(res));
+
+        fmt::print("{}\n", message);
+        return ControllerResponse { false, message };
+    }
+
+    return ControllerResponse {  };
+}
+
+GetConfigResponse CameraController::get_config_item(const char *name) {
+    return GetConfigResponse {  };
+}
+
+
--- src/CameraController.h
+++ src/CameraController.h
@@ -1,9 +1,19 @@
 #pragma once
 
 #include <iostream>
+#include <fmt/core.h>
 #include <gphoto2/gphoto2-camera.h>
 #include <gphoto2/gphoto2-result.h>
 
+struct ControllerResponse {
+    bool successful = true;
+    std::string message = "Default Message";
+};
+
+struct GetConfigResponse : ControllerResponse {
+    const void *value = nullptr;
+    const void **values = nullptr;
+};
 
 class CameraController {
 private:
@@ -11,6 +21,9 @@ private:
     GPContext *_context = nullptr;
 
 public:
-    bool connect();
-    bool disconnect();
+    ControllerResponse connect();
+    ControllerResponse disconnect();
+
+    ControllerResponse set_config_item(const char *name, const void *value);
+    GetConfigResponse get_config_item(const char *name);
 };
