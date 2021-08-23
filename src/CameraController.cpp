4514b47390637b396bab9bfe94483b95155cbf74
Dominic Maas <dominic.maas@live.com>
2021-08-24 10:18:37 +1200

    work on getting config

--- src/CameraController.cpp
+++ src/CameraController.cpp
@@ -17,14 +17,14 @@ ControllerResponse CameraController::connect() {
         auto message = fmt::format("Unable to connect to camera. Result: {}", gp_result_as_string(init_res));
 
         fmt::print("{}\n", message);
-        return ControllerResponse { false, message };
+        return ControllerResponse{false, message};
     }
 
     CameraText cam_text;
     gp_camera_get_about(this->_camera, &cam_text, this->_context);
 
     std::cout << "Camera Text: " << cam_text.text << std::endl;
-    return ControllerResponse {  };
+    return ControllerResponse{};
 }
 
 ControllerResponse CameraController::disconnect() {
@@ -32,7 +32,7 @@ ControllerResponse CameraController::disconnect() {
         gp_camera_exit(this->_camera, this->_context);
     }
 
-    return ControllerResponse {  };
+    return ControllerResponse{};
 }
 
 ControllerResponse CameraController::set_config_item(const char *name, const void *value) {
@@ -46,26 +46,28 @@ ControllerResponse CameraController::set_config_item(const char *name, const voi
         auto message = fmt::format("Unable to get camera config. Result: {}", gp_result_as_string(res));
 
         fmt::print("{}\n", message);
-        return ControllerResponse { false, message };
+        return ControllerResponse{false, message};
     }
 
     // Get the widget
     CameraWidget *widget;
     res = gp_widget_get_child_by_name(config, name, &widget);
     if (res != 0) {
-        auto message = fmt::format("Unable to get specified camera config ({}). Result: {}", name, gp_result_as_string(res));
+        auto message = fmt::format("Unable to get specified camera config ({}). Result: {}", name,
+                                   gp_result_as_string(res));
 
         fmt::print("{}\n", message);
-        return ControllerResponse { false, message };
+        return ControllerResponse{false, message};
     }
 
     // Attempt to set the value
     res = gp_widget_set_value(widget, value);
     if (res != 0) {
-        auto message = fmt::format("Unable to set camera config ({}) value. Result: {}", name, gp_result_as_string(res));
+        auto message = fmt::format("Unable to set camera config ({}) value. Result: {}", name,
+                                   gp_result_as_string(res));
 
         fmt::print("{}\n", message);
-        return ControllerResponse { false, message };
+        return ControllerResponse{false, message};
     }
 
     // Attempt to set the config
@@ -74,14 +76,50 @@ ControllerResponse CameraController::set_config_item(const char *name, const voi
         auto message = fmt::format("Unable to set camera config. Result: {}", gp_result_as_string(res));
 
         fmt::print("{}\n", message);
-        return ControllerResponse { false, message };
+        return ControllerResponse{false, message};
     }
 
-    return ControllerResponse {  };
+    return ControllerResponse{};
 }
 
 GetConfigResponse CameraController::get_config_item(const char *name) {
-    return GetConfigResponse {  };
+    // Keep track of return codes
+    int res;
+
+    // Get the config
+    CameraWidget *config;
+    res = gp_camera_get_config(this->_camera, &config, this->_context);
+    if (res != 0) {
+        auto message = fmt::format("Unable to get camera config. Result: {}", gp_result_as_string(res));
+
+        fmt::print("{}\n", message);
+        return GetConfigResponse{false, message};
+    }
+
+    // Get the widget
+    CameraWidget *widget;
+    res = gp_widget_get_child_by_name(config, name, &widget);
+    if (res != 0) {
+        auto message = fmt::format("Unable to get specified camera config ({}). Result: {}", name,
+                                   gp_result_as_string(res));
+
+        fmt::print("{}\n", message);
+        return GetConfigResponse{false, message};
+    }
+
+    void *value;
+    res = gp_widget_get_value(widget, &value);
+    if (res != 0) {
+        auto message = fmt::format("Unable to get specified camera config value ({}). Result: {}", name,
+                                   gp_result_as_string(res));
+
+        fmt::print("{}\n", message);
+        return GetConfigResponse{false, message};
+    }
+
+    fmt::print("Retrieved config value of {}!\n", value);
+
+    return GetConfigResponse{.value = value};
 }
 
 
