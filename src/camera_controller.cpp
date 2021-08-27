3e27d41bc5796b67c57c26f0e650fdd8cfe9e436
Dominic Maas <dominic.maas@live.com>
2021-08-27 19:23:33 +1200

    ahhhh 4

--- src/camera_controller.cpp
+++ src/camera_controller.cpp
@@ -134,8 +134,15 @@ GetConfigResponse CameraController::get_config_item(const std::string &name) {
         return GetConfigResponse{false, message};
     }
 
+    CameraWidgetType type;
+    gp_widget_get_type(widget, &type);
+
+    fmt::print("Type is {}", type);
+
     auto values = std::vector<std::string>();
     auto choice_count = gp_widget_count_choices(widget);
+
+    fmt::print("Choice count is {}", choice_count);
 
     for (int i = 0; i < choice_count; i++) {
         const char *raw_choice;
